5f2ebef269f0f0da326de98c2e939093ac15c9a0
nidhogg5 <nidhogg5@yahoo.com.tw>
2016-11-29 21:12:19 +0800

    伺服器版比大小
    實驗中

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+handle["/game.js"] = requestHandlers.game; 
+handle["/pic"] = requestHandlers.pic;
+
+server.start(router.route, handle);
