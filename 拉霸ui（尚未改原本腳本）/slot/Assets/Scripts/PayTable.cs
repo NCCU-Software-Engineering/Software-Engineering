b1e107ead60db8daa0a6aacc37eebb02fea4d5cc
IF Chen <103703007@nccu.edu.tw>
2016-12-24 22:08:03 +0800

    UI修好了
    但他玩法實在是太詭異了可能要修一下
    我在研究當中

---
+++ 拉霸ui（尚未改原本腳本）/slot/Assets/Scripts/PayTable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PayTable : MonoBehaviour {
+    public Sprite[] sprites;
+
+    public void DrawPayTable(GameObject payItemPrefab)
+    {
+        for (int i = sprites.Length-1; i >=0 ; i--)
+        {
+            GameObject go = Instantiate(payItemPrefab) as GameObject;
+            Transform tr = go.transform;
+            tr.SetParent(transform);
+            tr.localScale = Vector3.one;
+            tr.localPosition = Vector3.zero;
+            PayItem pItem = go.GetComponent<PayItem>();
+            pItem.sprite.sprite = sprites[i];
+            pItem.labels[0].text = "5x - " + PayData.table[i, 4];
+            pItem.labels[1].text = "4x - " + PayData.table[i, 3];
+            pItem.labels[2].text = "3x - " + PayData.table[i, 2];
+            pItem.labels[3].text = "";
+        }
+    }
+	
+	void Update () {
+	
+	}
+}
---
+++ 拉霸ui（尚未改原本腳本）/slot/Assets/Scripts/Tile.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Holoville.HOTween;
+using Holoville.HOTween.Plugins;
+
+/// <summary>
+/// Tile cell effect and touch/click event.
+/// </summary>
+public class Tile : MonoBehaviour {
+    public SlotMachine slotMachine;
+
+    // tile order
+    public int idx = 0;
+
+    public float height = 1f;
+
+    // tile type
+    int _type = 0;
+
+    Transform tf;
+
+    public Sprite[] sprites;
+    SpriteRenderer shapeRenderer;
+    SpriteRenderer choideRenderer;
+
+    // Tile Line Component
+    public Line lineScript;
+
+    // Condition when tile is moving
+    public bool isMove = false;
+
+    void Awake()
+    {
+        tf = transform;
+        shapeRenderer = tf.FindChild("Shape").GetComponent<SpriteRenderer>();
+        choideRenderer = tf.FindChild("Choice").GetComponent<SpriteRenderer>();
+        UnSetChoice();
+    }
+
+    // Setup Tile Type.
+    public void SetTileType(int type)
+    {
+        _type = type;
+        shapeRenderer.sprite = sprites[type];
+    }
+
+    public void SetChoice()
+    {
+        choideRenderer.enabled = true;
+    }
+
+    public void UnSetChoice()
+    {
+        choideRenderer.enabled = false;
+    }
+
+    // Get Tile Type.
+    public int GetTileType()
+    {
+        return _type;
+    }
+
+    public void ShowChoice(float t)
+    {
+        SetChoice();
+        StartCoroutine(DelayAction(t, () =>
+        {
+            UnSetChoice();
+        }));
+        shapeRenderer.transform.localScale = Vector3.one;
+        TweenParms parms = new TweenParms().Prop("localScale", Vector3.one * 1.1f).Ease(EaseType.EaseOutElastic).Loops(2,LoopType.Yoyo);
+        HOTween.To(shapeRenderer.transform, 0.1f, parms);
+    }
+
+    // Move To Order Position
+    public void MoveTo(int seq)
+    {
+        tf.localPosition = Vector3.forward * (seq * height);
+    }
+
+    // Move with Tweening Animation
+    public void TweenMoveTo(int seq, EaseType easeType)
+    {
+        TweenMove(tf, tf.localPosition, Vector3.forward * (seq * height), easeType);
+    }
+
+    // Move with Tweening Animation
+    void TweenMove(Transform tr, Vector3 pos1, Vector3 pos2, EaseType easeType)
+    {
+        tr.localPosition = pos1;
+        TweenParms parms = new TweenParms().Prop("localPosition", pos2).Ease(easeType);
+        HOTween.To(tr, 0.1f, parms);
+    }
+
+    // Coroutine Delay Method
+    IEnumerator DelayAction(float dTime, System.Action callback)
+    {
+        yield return new WaitForSeconds(dTime);
+        callback();
+    }
+}
