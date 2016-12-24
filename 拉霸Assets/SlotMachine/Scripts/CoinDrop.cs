a10dbdcad40100f5abaaa16074117888e7931875
nidhogg5 <nidhogg5@yahoo.com.tw>
2016-12-24 20:41:02 +0800

    拉霸資料

---
+++ 拉霸Assets/SlotMachine/Scripts/CoinDrop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinDrop : MonoBehaviour {
+    public float delayTime = 1f;
+    SpriteRenderer sr;
+    Animator animator;
+
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+	}
+
+    public void ShowMotion()
+    {
+        sr.enabled = true;
+        animator.Play("CoinDrop");
+        StartCoroutine(DelayAction(delayTime, () =>
+        {
+            StopMotion();
+        }));
+    }
+
+    public void StopMotion()
+    {
+        sr.enabled = false;
+        animator.Play("Stop");
+    }
+
+    IEnumerator DelayAction(float dTime, System.Action callback)
+    {
+        yield return new WaitForSeconds(dTime);
+        callback();
+    }
+}
---
+++ 拉霸Assets/SlotMachine/Scripts/PayItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PayItem : MonoBehaviour {
+    public Image sprite;
+    public Text[] labels;
+}
